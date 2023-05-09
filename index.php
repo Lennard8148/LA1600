6894e8ad6dff7a1233d53bc18e84589886141232
Timog <110891995+timog06@users.noreply.github.com>
2023-05-09 08:32:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cute Cats World</title>
+</head>
+<body>
+    <header>
+        <h1>Cute Cats World</h1>
+    </header>
+
+    <main>
+        <div>
+            <img src="https://example.com/cat1.jpg" alt="Cute Cat 1">
+            <h2>Fluffy</h2>
+        </div>
+        <div>
+            <img src="https://example.com/cat2.jpg" alt="Cute Cat 2">
+            <h2>Muffin</h2>
+        </div>
+        <div>
+            <img src="https://example.com/cat3.jpg" alt="Cute Cat 3">
+            <h2>Whiskers</h2>
+        </div>
+        <div>
+            <img src="https://example.com/cat4.jpg" alt="Cute Cat 4">
+            <h2>Luna</h2>
+        </div>
+    </main>
+
+    <footer>
+        &copy; 2023 Cute Cats World. All Rights Reserved.
+    </footer>
+</body>
+</html>
